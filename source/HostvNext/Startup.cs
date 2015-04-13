769685a13844abe77a0528e21a6d4b903c01a0c4
mwmartin <mwm.cal@gmail.com>
2015-04-13 15:42:59 -0600

    removing identityproviders codes for now

--- source/HostvNext/Startup.cs
+++ source/HostvNext/Startup.cs
@@ -22,11 +22,6 @@ using Microsoft.Framework.Logging;
 using Microsoft.Framework.Logging.Console;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.DataProtection;
-using Microsoft.Owin.Security.Facebook;
-using Microsoft.Owin.Security.Google;
-using Microsoft.Owin.Security.OpenIdConnect;
-using Microsoft.Owin.Security.Twitter;
-using Microsoft.Owin.Security.WsFederation;
 using Owin;
 using IdentityServer3.Core.Configuration;
 using IdentityServer3.Core.Logging;
@@ -155,62 +150,6 @@ namespace IdentityServer3.HostvNext
 
         public static void ConfigureIdentityProviders(IAppBuilder app, string signInAsType)
         {
-            var google = new GoogleOAuth2AuthenticationOptions
-            {
-                AuthenticationType = "Google",
-                Caption = "Google",
-                SignInAsAuthenticationType = signInAsType,
-                
-                ClientId = "767400843187-8boio83mb57ruogr9af9ut09fkg56b27.apps.googleusercontent.com",
-                ClientSecret = "5fWcBT0udKY7_b6E3gEiJlze"
-            };
-            app.UseGoogleAuthentication(google);
-
-            var fb = new FacebookAuthenticationOptions
-            {
-                AuthenticationType = "Facebook",
-                Caption = "Facebook",
-                SignInAsAuthenticationType = signInAsType,
-                
-                AppId = "676607329068058",
-                AppSecret = "9d6ab75f921942e61fb43a9b1fc25c63"
-            };
-            app.UseFacebookAuthentication(fb);
-
-            var twitter = new TwitterAuthenticationOptions
-            {
-                AuthenticationType = "Twitter",
-                Caption = "Twitter",
-                SignInAsAuthenticationType = signInAsType,
-                
-                ConsumerKey = "N8r8w7PIepwtZZwtH066kMlmq",
-                ConsumerSecret = "df15L2x6kNI50E4PYcHS0ImBQlcGIt6huET8gQN41VFpUCwNjM"
-            };
-            app.UseTwitterAuthentication(twitter);
-
-            var adfs = new WsFederationAuthenticationOptions
-            {
-                AuthenticationType = "adfs",
-                Caption = "ADFS",
-                SignInAsAuthenticationType = signInAsType,
-
-                MetadataAddress = "https://adfs.leastprivilege.vm/federationmetadata/2007-06/federationmetadata.xml",
-                Wtrealm = "urn:idsrv3"
-            };
-            app.UseWsFederationAuthentication(adfs);
-
-            var aad = new OpenIdConnectAuthenticationOptions
-            {
-                AuthenticationType = "aad",
-                Caption = "Azure AD",
-                SignInAsAuthenticationType = signInAsType,
-
-                Authority = "https://login.windows.net/4ca9cb4c-5e5f-4be9-b700-c532992a3705",
-                ClientId = "65bbbda8-8b85-4c9d-81e9-1502330aacba",
-                RedirectUri = "https://localhost:44333/core/aadcb"
-            };
-
-            app.UseOpenIdConnectAuthentication(aad);
         }
     }
 }
